4d1aa01cc3f4e2ac91c43d430728837e343e971c
James Childers <james.childers@gmail.com>
2021-11-18 21:13:36 -0600

    nvim: lua cfg cleanup

--- nvim/after/ftplugin/ruby.vim
+++ nvim/after/ftplugin/ruby.vim
@@ -3,5 +3,5 @@ set shiftwidth=2
 
 nnoremap <Leader>bp obinding.pry<ESC>:w<ENTER>
 nnoremap <Leader>bP Obinding.pry<ESC>:w<ENTER>
-nnoremap <Leader>rp oputs "-=-=> #{__method__}:#{__LINE__} -> "<ESC>i
-nnoremap <Leader>rP Oputs "-=-=> #{__method__}:#{__LINE__} -> "<ESC>i
+nnoremap <Leader>rp oputs "-=-=> #{self.class}##{__method__}:#{__LINE__}\n\t-> "<ESC>i
+nnoremap <Leader>rP Oputs "-=-=> #{self.class}##{__method__}:#{__LINE__}\n\t-> "<ESC>i
--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -6,3 +6,32 @@ vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 vim.g.loaded_ruby_provider = 0
 
 require "jc.globals"
+
+local opt = vim.opt
+
+opt.expandtab = true
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.tabstop = 4
+opt.smarttab = true
+
+opt.backup = false
+opt.swapfile = false
+-- commenting this out until I can fix this: ~ is creating a directory literally named "~"
+--[[ opt.undodir = '~/.local/share/nvim/backups'
+opt.undofile = false ]]
+
+opt.autoindent = true
+opt.fileformat = 'unix'
+opt.grepprg = 'rg --vimgrep --no-heading --hidden'
+opt.hidden = true
+opt.hlsearch = true
+opt.inccommand = 'split'
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 5
+opt.sidescrolloff = 15
+opt.sidescroll = 5
+opt.signcolumn = 'yes'
+opt.smartcase = true
+opt.wrap = true
