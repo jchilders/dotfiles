eee5eedcd45a9f9a10a6f8ddc235da22123dafc1
James Childers <james.childers@gmail.com>
2021-11-19 18:48:36 -0600

    more harpoon stuff

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 - [ ] nvim: Harpoon
 - [ ] nvim: Create mapping to switch to alternate (test) file using `alt`
   command from homebrew
+  - [vim-test](https://github.com/vim-test/vim-test) looks good
 - [ ] nvim: Get <leader>sl to work with text objects. I.e.: `<leader>sl3j` will
   send curr line + 3 to the left
 - [ ] zsh: better flow after ^ogd to overwrite/reset file from HEAD (discard local chgs)
--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -3,9 +3,9 @@
 
 local map_options = {}
 -- add key file for this project (harpoon)
-vim.api.nvim_set_keymap("n", "<Leader>af", "<cmd>lua R('harpoon.mark').add_file()<CR>", map_options)
--- open key files menu for this project (harpoon)
-vim.api.nvim_set_keymap("n", "<Leader>kf", "<cmd>lua R('harpoon.ui').toggle_quick_menu()<CR>", map_options)
+vim.api.nvim_set_keymap("n", "<Leader>ka", "<cmd>lua R('harpoon.mark').add_file()<CR>", map_options)
+-- open list of files marked as harpooned
+vim.api.nvim_set_keymap("n", "<Leader>kl", "<cmd>lua R('harpoon.ui').toggle_quick_menu()<CR>", map_options)
 
 -- Cmd-j opens the first file added to the harpoon
 -- Cmd-k opens the second file added to the harpoon...
@@ -20,7 +20,10 @@ vim.api.nvim_set_keymap("n", "<C-;>", "<cmd>lua R('harpoon.ui').nav_file(4)<CR>"
 -- hmark.add_file()
 -- hui.nav_file(1)
 --[[ hterm = require("harpoon.term")
-hterm.gotoTerminal(1)
+
+lua require("harpoon.term").gotoTerminal(1)
+lua require("harpoon.term").sendCommand(1, 1)
+
  -- This will send to terminal 1 either the predefined command 1 in the terminal
  -- config or "ls -la"
 hterm.sendCommand(1, 1)
@@ -35,17 +38,24 @@ hterm.sendCommand(1, "ls -la") ]]
     },
 }) ]]
 
--- Project specific commands:
---
--- require("harpoon").setup({
---   projects = {
---   -- Yes $HOME works
---   ["$HOME/personal/vim-with-me/server"] = {
---     term = {
---       cmds = {
---         "./env && npx ts-node src/index.ts"
---       }
---     }
---   },
---  ]]
+-- PR adding tmux suupport:
+--   https://github.com/ThePrimeagen/harpoon/pull/103
 
+-- Project specific commands:
+
+require("harpoon").setup({
+  global_settings = {
+    enter_on_sendcmd = true,
+  },
+  projects = {
+    -- Yes $HOME works
+    ["$HOME/workspace/dva/vets-api"] = {
+      term = {
+        cmds = {
+          "bin/rails server",
+          "foreman start",
+        }
+      }
+    }
+  }
+})
