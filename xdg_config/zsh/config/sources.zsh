85a7325ddd58b96d7fd9df79dd644ead1b1615dc
James Childers <james.childers@gmail.com>
2023-10-15 15:00:40 -0400

    rust env fix

--- xdg_config/zsh/config/sources.zsh
+++ xdg_config/zsh/config/sources.zsh
@@ -7,9 +7,10 @@ source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zs
 source $HOMEBREW_PREFIX/share/zsh-abbr/zsh-abbr.zsh 
 
 # asdf is a version manager for Ruby, Node.js, etc.
-# [ -d "${HOMEBREW_PREFIX}/opt/asdf" ] && source "${HOMEBREW_PREFIX}"/opt/asdf/libexec/asdf.sh
 if [ -d $(brew --prefix asdf) ]; then
   export ASDF_DATA_DIR=$XDG_DATA_HOME/asdf
   export ASDF_GEM_DEFAULT_PACKAGES_FILE=$XDG_CONFIG_HOME/asdf/.default-gems
   . $(brew --prefix asdf)/libexec/asdf.sh
+
+  [ $(asdf list rust) ] && source "$ASDF_DATA_DIR/installs/rust/1.73.0/env"
 fi
