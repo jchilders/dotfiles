a2b178f4656b944b188a8d9d331496337b35c7a4
James Childers <james.childers@gmail.com>
2024-08-11 14:05:06 -0400

    nvim: set filetype based on shebang

---
+++ xdg_config/nvim/lua/filetype.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_augroup("shebang_filetype", { clear = true })
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  group = "shebang_filetype",
+  pattern = "*",
+  callback = function()
+    local shebang = vim.fn.getline(1)
+    if shebang:match('^#!.*%bruby%b') then
+      vim.bo.filetype = 'ruby'
+    elseif shebang:match('^#!.*%bzsh%b') then
+      vim.bo.filetype = 'zsh'
+    elseif shebang:match('^#!.*%bpython%b') then
+      vim.bo.filetype = 'python'
+    elseif shebang:match('^#!.*%bbash%b') then
+      vim.bo.filetype = 'sh'
+    elseif shebang:match('^#!.*%bnode%b') then
+      vim.bo.filetype = 'javascript'
+    elseif shebang:match('^#!.*%bosascript%b') then
+      vim.bo.filetype = 'applescript'
+    end
+  end,
+})
